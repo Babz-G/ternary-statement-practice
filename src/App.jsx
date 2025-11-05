2a54afd9236d3b8d72905f0aa730060f05a6fc28
Babz-G <barbaramgaynor@gmail.com>
2025-11-04 21:37:31 -0500

    centered cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import ProductCard from "./components/ProductCard.jsx";
 
 function App() {
   return (
-    <div>
-      <ProductCard productName="Vintage Tactical Military Backpack" />
-      <ProductCard productName="Dr. Martens 8-Eye Burgandy Vegan Boot" />
-      <ProductCard productName="Black & Leopard Vegan Creepers" />
-      <ProductCard productName="Vans Slip-On Black / White Checkerboard Skate Shoe" />
-    </div>
+    <>
+      <h1>Ternary Statement Practice</h1>
+      <div className="card-container">
+        <ProductCard productName="Vintage Tactical Military Backpack" />
+        <ProductCard productName="Dr. Martens 8-Eye Burgandy Vegan Boot" />
+        <ProductCard productName="Black & Leopard Vegan Creepers" />
+        <ProductCard productName="Vans Slip-On Black / White Checkerboard Skate Shoe" />
+      </div>
+    </>
   );
 }
 export default App;
